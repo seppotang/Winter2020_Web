3346fe7f6746ffdba29fdd5fc9f788e7bee0deec
seppotang <mjgraves91@gmail.com>
2020-05-07 18:53:50 -0400

    Test code to see if I can connect to Server

---
+++ Assignment 5/HelloWorld.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var express = require('express'),
+    exphbs  = require('../../'); // "express-handlebars"
+
+var app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+app.listen(9001, function () {
+    console.log('express-handlebars example server listening on: 9001');
+});
