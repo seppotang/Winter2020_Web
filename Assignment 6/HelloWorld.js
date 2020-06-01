f3903107f9f4f193c5dcf3cf150d3999c62ed41a
seppotang <mjgraves91@gmail.com>
2020-06-01 17:05:08 -0400

    removed handlebars refs

--- Assignment 6/HelloWorld.js
+++ Assignment 6/HelloWorld.js
@@ -1,31 +1,9 @@
 'use strict';
 
-var express = require('express'),
-    exphbs  = require('express-handlebars'); // "express-handlebars"
-    mysql   = require('mysql)');             // "mysql"
+var mysql   = require('mysql)');             // "mysql"
     pool    = mysql.createPool({
         host    :   'localhost',
         user    :   'student',
         password:   'default',
         database:   'student'
     });
-var app = express();
-
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
-
-app.get('/', function (req, res) {
-    res.render('home');
-});
-
-app.listen(9001, function () {
-    console.log('express-handlebars example server listening on: 9001');
-});
-
-function tableEdit(tableEntry){
-    
-}
-
-function tableDelete(tableEntry){
-    
-}
